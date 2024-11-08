3beeb823803fe1753482ea0188022b819a903f44
rustemdastanbeisenov <230714@astanait.edu.kz>
2024-11-08 14:38:21 +0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,22 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
     const user = JSON.parse(localStorage.getItem("user"));
+    const loginForm = document.getElementById("loginForm");
+    const logoutBtn = document.getElementById("logoutBtn");
+    const loginMessage = document.getElementById("loginMessage");
+
     if (user) {
         displayWelcomeMessage(user.username);
     }
+
     loginForm.addEventListener("submit", function(event) {
         event.preventDefault();
         const username = document.getElementById("username").value;
         const password = document.getElementById("password").value;
-        
+
         const userData = { username: username };
         localStorage.setItem("user", JSON.stringify(userData));
         displayWelcomeMessage(username);
     });
+
     logoutBtn.addEventListener("click", function() {
         localStorage.removeItem("user");
         loginMessage.textContent = "";
         logoutBtn.style.display = "none";
+        loginForm.style.display = "block";
     });
+
     function displayWelcomeMessage(username) {
         loginMessage.textContent = `Welcome, ${username}!`;
         logoutBtn.style.display = "block";
