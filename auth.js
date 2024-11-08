b77664da8ef3955729337ca2167a238c15194a29
rustemdastanbeisenov <230714@astanait.edu.kz>
2024-11-08 14:45:42 +0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,19 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const user = JSON.parse(localStorage.getItem("user"));
+document.addEventListener("DOMContentLoaded", function() { 
     const loginForm = document.getElementById("loginForm");
     const logoutBtn = document.getElementById("logoutBtn");
     const loginMessage = document.getElementById("loginMessage");
-
-    if (user) {
-        displayWelcomeMessage(user.username);
+    const registerBtn = document.getElementById("register");
+    
+    const storedUser = localStorage.getItem("user");
+    
+    if (storedUser) {
+		const user = JSON.parse(storedUser);
+		displayWelcomeMessage(user.username);
+    } else {
+        logoutBtn.style.display = "none";
+        loginForm.style.display = "block";
     }
 
     loginForm.addEventListener("submit", function(event) {
         event.preventDefault();
         const username = document.getElementById("username").value;
         const password = document.getElementById("password").value;
-
-        const userData = { username: username };
+        
+        const userData = { username: username, password: password };
         localStorage.setItem("user", JSON.stringify(userData));
         displayWelcomeMessage(username);
     });
@@ -21,8 +27,8 @@ document.addEventListener("DOMContentLoaded", function() {
     logoutBtn.addEventListener("click", function() {
         localStorage.removeItem("user");
         loginMessage.textContent = "";
-        logoutBtn.style.display = "none";
         loginForm.style.display = "block";
+        logoutBtn.style.display = "none";
     });
 
     function displayWelcomeMessage(username) {
