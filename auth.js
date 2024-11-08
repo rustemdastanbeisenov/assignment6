12cce01b0fe973e85761d544771ce3da91d7a733
rustemdastanbeisenov <230714@astanait.edu.kz>
2024-11-08 14:15:06 +0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const user = JSON.parse(localStorage.getItem("user"));
-    if (user) {
-        displayWelcomeMessage(user.username);
+document.addEventListener("DOMContentLoaded", function() { 
+    const loginForm = document.getElementById("loginForm");
+    const logoutBtn = document.getElementById("logoutBtn");
+    const loginMessage = document.getElementById("loginMessage");
+
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+
+    if (storedUser) {
+        displayWelcomeMessage(storedUser.username);
+    } else {
+        logoutBtn.style.display = "none";
+        loginForm.style.display = "block";
     }
 
     loginForm.addEventListener("submit", function(event) {
@@ -9,18 +17,23 @@ document.addEventListener("DOMContentLoaded", function() {
         const username = document.getElementById("username").value;
         const password = document.getElementById("password").value;
 
-        if (username === "user" && password === "password") {
-            const userData = { username: username };
+        if (username === storedUser.username) {
+            if (username === storedUser.username && password === storedUser.password) {
+                displayWelcomeMessage(username);
+            } else {
+                loginMessage.textContent = "Invalid password.";
+            }
+        } else {
+            const userData = { username: username, password: password };
             localStorage.setItem("user", JSON.stringify(userData));
             displayWelcomeMessage(username);
-        } else {
-            loginMessage.textContent = "Invalid username or password.";
         }
     });
 
     logoutBtn.addEventListener("click", function() {
         localStorage.removeItem("user");
         loginMessage.textContent = "";
+        loginForm.style.display = "block";
         logoutBtn.style.display = "none";
     });
 
