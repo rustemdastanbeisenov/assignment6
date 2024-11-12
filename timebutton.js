58bd53ff9237d2791aa17bea7ee9a4882c305a08
rustemdastanbeisenov <230714@astanait.edu.kz>
2024-11-12 11:28:23 +0500

    Delete timebutton.js

--- timebutton.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('showTimeBtn').addEventListener('click', function () {
-	const currentTime = new Date().toLocaleTimeString();
-	document.getElementById('timeDisplay').textContent = `Current Time: ${currentTime}`;
-});
